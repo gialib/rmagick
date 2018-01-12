5d997b1a43ddd84babd245e82c35412934a0e110
WangJinzhong <andywang7259@163.com>
2018-01-12 17:59:54 +0800

    fix

--- lib/rmagick/version.rb
+++ lib/rmagick/version.rb
@@ -1,6 +1,6 @@
 module Magick
   VERSION = '2.16.0'
   MIN_RUBY_VERSION = '2.0.0'
-  MIN_IM_VERSION = '6.9.9'
-  MIN_WAND_VERSION = '6.9.9'
+  MIN_IM_VERSION = '7.0.0'
+  MIN_WAND_VERSION = '7.0.0'
 end
